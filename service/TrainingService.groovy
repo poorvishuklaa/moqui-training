0e8b8c91172bb7477d20ec704743546b47f1917c
Poorvi shukla <shuklapoorvi26@gmail.com>
2024-12-30 00:26:05 +0530

    RESTful APIs for Order Data

--- service/TrainingService.groovy
+++ service/TrainingService.groovy
@@ -1,4 +1,13 @@
 def training = ec.entity.makeValue("MoquiTraining")
-training.setFields(context, true, null, null)
-training.setSequencedIdPrimary()
-training.create()
+training.setFields(context, true, null, null) //  entity object with values from the context.
+training.setSequencedIdPrimary() //Sets a unique primary key
+training.create() //inserts new record into database
+
+// Groovy code snippet is used in the Moqui framework to
+// create a new record in the MoquiTraining entity
+ def training = ec.entity.makeValue("MoquiTraining")
+//Purpose: Creates a new entity value object for the MoquiTraining entity.
+//Explanation:
+//ec.entity: This is the EntityFacade object from the Moqui framework,
+// which provides methods for interacting with entities.
+//makeValue("MoquiTraining"): Initializes a new instance of the MoquiTraining entity with no values set yet.
